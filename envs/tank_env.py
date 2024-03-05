507255e598b0eda0eeb17ffc65df39c621ea0548
Dabere Abasse <dabereabasse@MacBook-Air-de-Dabere.local>
2024-03-05 17:32:48 +0100

    initialisation de l'environnement

--- envs/tank_env.py
+++ envs/tank_env.py
@@ -9,18 +9,30 @@ import numpy as np
 class TankEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
+    def __init__(self, max_x, max_y, max_enemies):
         super(TankEnv, self).__init__()
-        self.action_space = spaces.Discrete(3)
-        self.observation_space = spaces.Box(low=0, high=255, shape=(84, 84, 3), dtype=np.uint8)
-        # Initialisation de l'état du jeu
+        
+        self.max_x = max_x # Largeur de la grille
+        self.max_y = max_y # Hauteur de la grille
+        self.max_enemies = max_enemies
+        self.max_projectiles = max_x * max_y
+
+        # Define action space
+        self.action_space = spaces.Discrete(6)  # stay, shoot, up, right, down, left
+        
+        # Define observation space
+        self.observation_space = spaces.Dict({
+            'player': spaces.Box(low=np.array([0, 0, 0]), high=np.array([self.max_x, self.max_y, 4]), dtype=np.int),  # x, y, direction
+            'enemies': spaces.Box(low=np.array([0, 0, 0]), high=np.array([self.max_x, self.max_y, 4]), dtype=np.int, shape=(self.max_enemies, 3)),  # x, y, direction
+            'projectiles': spaces.Box(low=np.array([0, 0, 0, 0]), high=np.array([self.max_x, self.max_y, 4, 1]), dtype=np.int, shape=(self.max_projectiles, 4))  # x, y, direction, from (0: player, 1: enemy)
+        })
+        
+    def reset(self):
+        # Implement the reset method here
+        pass
 
     def step(self, action):
         # Implement the step method here
-        pass
-
-    def reset(self):
-        # Implement the reset method here
         pass
 
     def render(self, mode='human'):
